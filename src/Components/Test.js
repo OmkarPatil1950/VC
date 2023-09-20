b61672b1fe91ca624efaa35f79120630dcd47058
OmkarPatil1950 <omkarpatil5607@gmail.com>
2023-09-20 16:36:48 +0530

    first commit

---
+++ src/Components/Test.js
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+function Test()
+{
+    return(
+        <>
+            <div className='d-flex justify-content-center align-items-center'>
+                <h1>Thanks For Using Anup's WebRTC Project !!!</h1>
+            </div>
+            <div className='d-flex justify-content-center align-items-center mt-5'>
+                <h3>You Can Close This Window...</h3>
+            </div>
+        </>
+    );
+}
+
+export default Test;
